54932c685f15c2104d3a7f27fe8b3349808fc44d
NickCulbertson <nickculbertson@gmail.com>
2025-03-09 19:03:31 -0400

    Removed input processing

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -57,7 +57,6 @@ AudioProcessorValueTreeState::ParameterLayout createParameterLayout()
 //==============================================================================
 ObxdAudioProcessor::ObxdAudioProcessor()
 	: AudioProcessor(BusesProperties()
-		.withInput("Main", AudioChannelSet::stereo(), true)
 		.withOutput("Main", AudioChannelSet::stereo(), true)),
 programs()
 	, bindings()
